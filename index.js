3e0d990e30e1ad268958e84d93a62d1ee36affda
abunts <arbunta@ad.unc.edu>
2021-06-15 23:40:56 -0400

    commit on 'index'

--- index.js
+++ index.js
@@ -1,47 +1,65 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer');
+//homebase url to grab heel life events
+const homeURL = 'https://heellife.unc.edu/events';
 
-const URL = 'https://heellife.unc.edu/events'
+//fetching events async function
+  const fetchEvents = async () => {
+    //launching browser using puppeteer
+  const browser = await puppeteer.launch();
+  //opening new page in browser
+  const page = await browser.newPage();
+  // use { waitUntil: 'networkidle2' } to have network request for 0.5 sec before proceeding
+  await page.goto(homeURL + '/events');
 
-
-// const fetchEvents = async function() {}
-const fetchEvents = async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-
-  // `networkIdle2` waits until there are no (<2 ?)
-  // network requests for 0.5 sec before proceeding
-  await page.goto(URL, { waitUntil: 'networkidle2' })
   const eventData = await page.evaluate(() => {
-    let events = []
-    const eventCards = [...document.querySelectorAll('.MuiPaper-root.MuiCard-root.MuiPaper-elevation3.MuiPaper-rounded')]
-    
-    eventCards.forEach(card => {
-      //pulling the titles for every event (under h3 element)
-      const title = card.querySelector('h3').innerText
-      //pulling the links (a) to get to specific events
-      const url = card.querySelector('a')
-      //pulling the description for every event 
-      //const description = card.getElementsByTagName('meta[og:description]')
-      const description = card.getElementsByClassName('DescriptionText')
-      for (var i = 0; i < description.length; i++) {
-        const d = description[i].innerText;
-        alert("description: " + d)
-    }
-      const newEvent = {
-        title: title,
-        url: url,
-        description: description,
-      }
-      events.push(newEvent)
-    })
-    return events
-  })
-
-  console.log(eventData)
-  
+    //creating an array for event urls
+    let urls = [];
+    //pulling the events from the main event page on heel life website
+    let anchorTags = document.querySelectorAll('#event-discovery-list a');
+    /* ADDED:
+    //after clicking on "load more" button
+    page.click('button'),
+    page.waitForSelector('button')
+    */
+    anchorTags.forEach((card) => {
+      //pulling url from specific event (getting its href) and adding it to event(s) array 
+      urls.push(card.getAttribute('href'));
+    });
+    return urls;
+  });
+//array holding other data for event info (description, title, etc.)
+  let data = [];
+  //gets urls of all event cards
+  for(const url of eventData){
+    //fulfills all of the events in the array
+    await Promise.all([
+      //waiting for the page to load everything
+      page.waitForNavigation(),
+      //redirecting to heel life events page and then a specific event
+      page.goto(homeURL + url),
+      page.waitForSelector('.engage-application'),
+    ]);
+  const pageData = await page.evaluate(() => {
+    //pulling title from the event card (found in h1 tag)
+  const title = document.querySelector('h1').innerText;
+  //pulling description from the event card (found in a div with class name DescriptionText)
+  const description = document.querySelector('.DescriptionText').innerText;
+  /* ADDED:
+  //sorting through events and pulling events related to data science 
+  const dataScience = document.document.querySelector('#event-discovery-list a[href*="data science="]');
+  if(document.contains(dataScience)){
+    data.push({...pageData, url: homeURL + url});
+  }*/
+  return {title, description};
+});
+//adding new info to the data array
+data.push({...pageData, url: homeURL + url});
 }
-
+  await browser.close();
+  console.log(data);
+  };
 
 //
 
-fetchEvents()
+fetchEvents(); 
+
